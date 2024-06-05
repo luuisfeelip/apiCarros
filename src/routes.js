3ee8a994441849e4240f331f703fd435dcbec549
Luis Felipe <luuisfeelip@gmail.com>
2024-06-05 10:03:39 -0300

    Finalizando apiCarros

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const CarroController = require('./controllers/CarroController');
+const CarroController = require('./controllers/CarroController') 
 
-router.get('/carros', CarroController.buscarTodos);
-module.exports = router;
+router.get('/carros', CarroController.buscarTodos)
+router.get('/carro/:codigo', CarroController.buscarUm)
+router.post('/carro', CarroController.inserir)
+router.put('/carro/:codigo', CarroController.alterar)
+router.delete('/carro/:codigo', CarroController.excluir)
+
+module.exports = router
--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -1,13 +1,83 @@
-const db = require('../db');
+const db = require('../db')
 
+db.query
 
 module.exports = {
-    buscarTodos: ()=> {
-        return new Promise((aceito,rejeitado)=>{
-            db.query('SELECT * FROM carros',(error,results)=>{
-                if (error){rejeitado(error);return;}
+    buscarTodos: () => {
+        return new Promise((aceito, rejeitado)=>{
+            db.query('SELECT * FROM carros', (error, results)=>{
+                if(error) { rejeitado(error); return;}
                 aceito(results);
-            });
-        });
+            })
+        })
+        
+    },
+
+    inserir: (modelo, placa)=> {
+        return new Promise((aceito, rejeitado)=> {
+
+            db.query('INSERT INTO carros (modelo, placa) VALUES (?, ?)',
+                [modelo, placa],
+                (error, results)=>{
+                    if(error){
+                        rejeitado(error)
+                        return
+                    }
+                    aceito(results.insertCodigo) //insertID
+                }
+            )
+        })
+    },
+    
+    buscarUm: (codigo) =>{
+        return new Promise((aceito, rejeitado)=>{
+
+            db.query('SELECT * FROM carros WHERE codigo = ?',
+                [codigo],
+                (error, results) =>{
+                    if(error){
+                        rejeitado(error)
+                        return
+                    }
+                    if(results.length > 0){ //verifica se retornou mais de 1 e pega o 1
+                        aceito(results[0])
+                    }else{
+                        aceito(false)
+                    }
+                }
+            )
+        })
+    },
+
+    alterar: (codigo, modelo, placa) =>{
+        return new Promise((aceito, rejeitado)=> {
+
+            db.query('UPDATE carros SET modelo = ?, placa = ? WHERE codigo = ?',
+                [modelo, placa, codigo],
+                (error, results) => {
+                    if(error){
+                        rejeitado(error)
+                        return
+                    }
+                    aceito(results)
+                }
+            )
+        })
+    },
+
+    excluir: (codigo) => {
+        return new Promise((aceito, rejeitado) =>{
+
+            db.query('DELETE FROM carros WHERE codigo = ?',
+                [codigo],
+                (error, results) =>{
+                    if(error){
+                        rejeitado(error)
+                        return
+                    }
+                    aceito(results)
+                }
+            )
+        })
     }
-};
+}
